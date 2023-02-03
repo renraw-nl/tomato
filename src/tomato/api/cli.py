7550de405b0603d3120336c5267bf1dbf5d4f023
Warner Leijenaar <git@renraw.nl>
2023-02-03 23:23:55 +0100

    typing, docs and multiple corrections

--- src/tomato/api/cli.py
+++ src/tomato/api/cli.py
@@ -1,16 +1,15 @@
 """
-Tomato - a timetracker application
+Tomato - a CLI for a timetracker application
 """
 import os
 from pathlib import Path
-from typing import Optional
 
 import structlog
 import typer
 
 import tomato
 
-app = typer.Typer(
+app: typer.Typer = typer.Typer(
     add_help_option=True,
     help=__doc__,
     add_completion=True,
@@ -20,41 +19,61 @@ app = typer.Typer(
 
 
 def _version_callback(value: bool) -> None:
+    """Print version and stop."""
+
     if value:
-        print(f"CLI API for: {tomato.__app_name__}.{tomato.__version__=}")
+        print(f"CLI API for '{tomato.__app_name__}', version: {tomato.__version__}")
         raise typer.Exit()
 
 
-def _log_level(value: str) -> str:
-    if not tomato.log.loglevel_from_str(value):
-        raise typer.BadParameter(
-            "Verbose level should be one of `DEBUG`, `INFO`,"
-            "`WARNING`, `ERROR`. Exiting."
-        )
+def _log_level_callback(value: str) -> str | None:
+    """Typer callback for passed log level string."""
 
-    return value
+    if not value:
+        # Fail quick on empties
+        return None
+    elif (
+        isinstance(value, str)
+        and (value := value.strip().upper())
+        and tomato.log.loglevel_from_str(value)
+    ):
+        # A non-empty string matching a log level
+        return value
+
+    raise typer.BadParameter(
+        "Verbose level should be one of `DEBUG`, `INFO`," "`WARNING`, `ERROR`. Exiting."
+    )
 
 
-def _log_file(fn: str | Path) -> Path | None:
+def _log_file_callback(fn: str | Path) -> Path | None:
+    """Typer callback for passed logfile."""
+
     if not fn:
+        # Fail quick on empty arguments
         return None
 
-    if not isinstance(fn, Path):
+    elif isinstance(fn, str) and (fn := fn.strip()):
+        # A string and not an empty one.
         fn = Path(fn)
+
+    if not isinstance(fn, Path):
+        # Something was passed, but not resembling a Path
+        raise typer.BadParameter(f'Log file: "{fn}" is not usable as a file path.')
 
     fn = fn.expanduser().resolve()
     dn = fn.parent
+
     if fn.exists() and not os.access(fn, os.W_OK):
         raise typer.BadParameter(
-            f"Log file: {fn} exists, but is not writable. Existing."
+            f"Log file: {fn} exists, but is not writable. Exiting."
         )
     elif not dn.exists():
         raise typer.BadParameter(
-            f"Directory for log file does not exist: {dn}. Existing."
+            f"Directory for log file does not exist: {dn}. Exiting."
         )
     elif not os.access(dn, os.W_OK):
         raise typer.BadParameter(
-            f"Directory for log file: {dn} exists, but is not writable. Existing."
+            f"Directory for log file: {dn} exists, but is not writable. Exiting."
         )
 
     return fn
@@ -62,19 +81,22 @@ def _log_file(fn: str | Path) -> Path | None:
 
 @app.callback()
 def main(
-    log: Optional[str] = typer.Option(
+    log: str
+    | None = typer.Option(
         "INFO",
         "--log",
         help="Set the log and verbosity level [DEBUG, INFO, WARNING, ERROR].",
-        callback=_log_level,
+        callback=_log_level_callback,
     ),
-    logfile: Optional[Path] = typer.Option(
+    logfile: Path
+    | None = typer.Option(
         None,
         "--logfile",
         help="Log file to use, must be writable.",
-        callback=_log_file,
+        callback=_log_file_callback,
     ),
-    version: Optional[bool] = typer.Option(
+    version: bool
+    | None = typer.Option(
         None,
         "--version",
         help="Print version information and exit.",
@@ -86,7 +108,7 @@ def main(
 
     This callback is always run before any command.
     """
-    tomato.init(loglevel=log, logfile=logfile)
+    tomato.init(loglevel_name=log, logfile=logfile)
 
     logger: structlog.BoundLogger = structlog.get_logger(tomato.__app_name__)
     logger.debug("CLI initialised")
